1b70004cd735f12c0a17106155d3ea474e28c7af
omiras <oscar.miras@gmail.com>
2022-04-11 14:14:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import PanelLetters from './components/PanelLetters.vue';
+</script>
+
+<script>
+
+export default {
+  data() {
+    return {
+      guessedLetters: ['a', 't', 'r'],
+      movie: "Star Wars"
+    }
+  }
+}
+
+
+
+</script>
+
+<template>
+  <h1>Cine de Barrio</h1>
+  <main>
+    <panel-letters :text="movie" :guessedLetters="guessedLetters" />
+  </main>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #app {
+
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/components/PanelLetters.vue
@@ -0,0 +1,40 @@
+
+
+<script>
+export default {
+    props: ["text", "guessedLetters"]
+}
+</script>
+
+<template>
+    <div>
+        <span :class="{ 'empty': letter == ' ' }" v-for="(letter, index) in text" :key="index">
+            <span class="letter" v-if="guessedLetters.includes(letter)"> {{ letter }}
+            </span>
+            <span class="letter" v-else>
+            </span>
+        </span>
+    </div>
+</template>
+
+<style scoped>
+.empty {
+    background-color: transparent;
+    border: none;
+}
+
+div {
+    display: flex;
+    text-transform: uppercase;
+}
+
+span.letter {
+    font-size: 1.2rem;
+    background-color: brown;
+    padding: 5px;
+    border: black solid 1px;
+    width: 50px;
+    text-align: center;
+    font-weight: 700;
+}
+</style>
