1cca7a3c44c21ee665f9766c18ecf9d72d711f22
ardelgad <adelgadoporras@gmail.com>
2022-04-22 09:39:57 +0200

    solutionWarning

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createI18n } from 'vue-i18n'
+import { createI18n } from 'vue-i18n/index'
 
 const i18n = createI18n({
     locale: navigator.language,
